fc4c487568959a1014acd0e0e8af6fdcaafc93bb
HAT Software Development Services <75292269+hatsoftware@users.noreply.github.com>
2021-02-26 17:44:38 +0800

    Add files via upload

--- refregion.js
+++ refregion.js
@@ -2,81 +2,103 @@ var ref_reg = [
   {
   "id":1,  
   "regCode":"01",
-  "regDesc":"Region 1"
+  "regDesc":"Region 1",
+  "regDesc2":"REGION I (ILOCOS REGION)"
   },
   {
   "id":2,
   "regCode":"02",
-  "regDesc":"Region 2"
+  "regDesc":"Region 2",
+  "regDesc2":"REGION II (CAGAYAN VALLEY)"
   },
   {
   "id":3,
   "regCode":"03",  
-  "regDesc":"Region 3"
+  "regDesc":"Region 3",
+  "regDesc2":"REGION III (CENTRAL LUZON)"
   },
   {
   "id":4,
   "regCode":"04",
-  "regDesc":"Region 4"
+  "regDesc":"Region 4",
+  "regDesc2":"REGION IV-A (CALABARZON)"
+  },
+  {
+  "id":17,
+  "regCode":"17",
+  "regDesc":"Region 4",
+  "regDesc2":"REGION IV-B (MIMAROPA)"
   },
   {
   "id":5,
   "regCode":"05",
-  "regDesc":"Region 5"
+  "regDesc":"Region 5",
+  "regDesc2":"REGION V (BICOL REGION)"
   },
   {
   "id":6,
   "regCode":"06",
-  "regDesc":"Region 6"
+  "regDesc":"Region 6",
+  "regDesc2":"REGION VI (WESTERN VISAYAS)"
   },
   {
   "id":7,
   "regCode":"07",
-  "regDesc":"Region 7"
+  "regDesc":"Region 7",
+  "regDesc2":"REGION VII (CENTRAL VISAYAS)"
   },
   {
   "id":8,
   "regCode":"08",
-  "regDesc":"Region 8"
+  "regDesc":"Region 8",
+  "regDesc2":"REGION VIII (EASTERN VISAYAS)"
   },
   {
   "id":9,
   "regCode":"09",
-  "regDesc":"Region 9"
+  "regDesc":"Region 9",
+  "regDesc2":"REGION IX (ZAMBOANGA PENINSULA)"
   },
   {
   "id":10,
   "regCode":"10",
-  "regDesc":"Region 10"
+  "regDesc":"Region 10",
+  "regDesc2":"REGION X (NORTHERN MINDANAO)"
   },
   {
   "id":11,
   "regCode":"11",
-  "regDesc":"Region 11"
+  "regDesc":"Region 11",
+  "regDesc2":"REGION XI (DAVAO REGION)"
   },
   {
   "id":12,
   "regCode":"12",
-  "regDesc":"Region 12"
+  "regDesc":"Region 12",
+  "regDesc2":"REGION XII (SOCCSKSARGEN)"
   },
   {
   "id":13,
   "regCode":"13",
-  "regDesc":"Region 13"
+  "regDesc":"Region 13",
+  "regDesc2":"NATIONAL CAPITAL REGION (NCR)"
   },
   {
   "id":14,
   "regCode":"14",
-  "regDesc":"Region 14"
+  "regDesc":"Region 14",
+  "regDesc2":"CORDILLERA ADMINISTRATIVE REGION (CAR)"
   },
   {
   "id":15,
   "regCode":"15",
-  "regDesc":"Region 15"
+  "regDesc":"Region 15",
+  "regDesc2":"AUTONOMOUS REGION IN MUSLIM MINDANAO (ARMM)"
   },
   {
   "id":16,
   "regCode":"16",
-  "regDesc":"Region 16"
+  "regDesc":"Region 16",
+  "regDesc2":"REGION XIII (Caraga)"
   }
 ];
