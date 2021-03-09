a88ebe5a1c3c0f3225da9bf1e5479361ebb15950
HAT Software Development Services <75292269+hatsoftware@users.noreply.github.com>
2021-03-09 13:42:23 +0800

    Add files via upload

--- app/SW_VOTEMON.js
+++ app/SW_VOTEMON.js
@@ -56,7 +56,32 @@ self.addEventListener('activate', e => {
   self.clients.claim();
 });
 
-
+addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;     // if valid response is found in cache return it
+        } else {
+          return fetch(event.request)     //fetch from internet
+            .then(function(res) {
+              return caches.open(cacheName)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());    //save the response for future
+                  return res;   // return the fetched data
+                })
+            })
+            .catch(function(err) {       // fallback mechanism
+              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+                .then(function(cache) {
+                  return cache.match('/offline.html');
+                });
+            });
+        }
+      })
+  );
+});     
+/*
 self.addEventListener('fetch', event => {
   // Let the browser do its default thing
   // for non-GET requests.
@@ -79,3 +104,5 @@ self.addEventListener('fetch', event => {
     return fetch(event.request);
   }());
 });
+*/
+
