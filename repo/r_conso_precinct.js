e9fd38c0dad1ae9d81851b6c9486b72f8560501d
HAT Software Development Services <75292269+hatsoftware@users.noreply.github.com>
2021-05-02 15:26:56 +0800

    Add files via upload

--- repo/r_conso_precinct.js
+++ repo/r_conso_precinct.js
@@ -1,5 +1,4 @@
 function proc_conso_precinct(){ 
-  alert(999);
   if(CURR_AXTYPE <4){
     snackBar('ACCESS DENIED...');
     return;
@@ -33,74 +32,141 @@ function print_proc_conso_precinct(){
   document.getElementById('prn_div').innerHTML=dtl;
   */
 
+ var pg_width=875-75;
+ var pg_height=1100-75;
+
   var dtl=
-  '<div id="main_repo_conso_precinct" class="cls_preview_repo" style="margin:0 auto;width:780px;height:auto;height:auto;font-size:12px;font-weight:bold;margin-bottom:10px;padding:10px;color:black;border:0px solid red;background:white;">'+
-  '</div>';
-  
+  '<div id="main_repo_conso_precinct" class="cls_preview_repo" style="margin:0 auto;width:'+pg_width+'px;height:auto;height:auto;font-size:12px;font-weight:bold;margin-bottom:10px;padding:10px;color:black;border:0px solid red;background:none;">'+
+  '</div>';  
   document.getElementById('prn_div').innerHTML=dtl;
 
-  do_page();
-  var lctr=10;
+  var pg=1;
+  var lctr=0;
   
   var aryBrgy=tmp_ref_brgy;
-  var dtl2='';
+  var result=[];
+  var dtl_line='';
   for(var i=0;i<aryBrgy.length;i++){    
     var brgyCode=aryBrgy[i]['brgyCode'];	
-    var brgyDesc=aryBrgy[i]['brgyDesc'];	    
+    var brgyDesc=aryBrgy[i]['brgyDesc'];	 
+    result=getData_conso_precinct(brgyCode);
+
+    var voters=result['voters'];
+    var votes=result['votes'];
+    var clusters=result['clusters'];
+
+    var perc=0;
+    if(votes > 0 && voters > 0){ perc=round(((votes/voters)*100),0); }
+    var vopen=result['vopen'];
+    var vclosed=result['vclosed'];
+    var vstatus='OPEN';
+    if(perc==100){ vstatus='CLOSED'; }
+
     //alert(brgyDesc);
-    dtl2+=    
-    '<div style="height:25px;width:100%;border:0px solid blue;background:none;">'+      
+    dtl_line+=    
+    '<div style="height:25px;width:100%;padding:0px;border:0px solid blue;background:none;">'+      
       '<div style="float:left;height:100%;width:30%;padding:4px;border:1px solid black;text-align:center;">'+brgyDesc+'</div>'+      
+      '<div style="float:left;height:100%;width:9.9%;padding:4px;border:1px solid black;text-align:right;">'+iif(voters>0,jformatNumber(voters),'')+'</div>'+
+      '<div style="float:left;height:100%;width:10.2%;padding:4px;border:1px solid black;text-align:right;">'+iif(votes>0,jformatNumber(votes),'')+'</div>'+
+      '<div style="float:left;height:100%;width:9.9%;padding:4px;border:1px solid black;text-align:right;">'+iif(clusters>0,jformatNumber(clusters),'')+'</div>'+
+
+      '<div style="float:left;height:100%;width:9.9%;padding:4px;border:1px solid black;text-align:center;">'+iif(perc>0,jformatNumber(perc)+'%','')+'</div>'+      
+      '<div style="float:left;height:100%;width:9.9%;padding:4px;border:1px solid black;text-align:right;">'+iif(vopen>0,jformatNumber(vopen),'')+'</div>'+      
+      '<div style="float:left;height:100%;width:10.2%;padding:4px;border:1px solid black;text-align:right;">'+iif(vclosed>0,jformatNumber(vclosed),'')+'</div>'+      
+      '<div style="float:left;height:100%;width:9.9%;padding:4px;border:1px solid black;text-align:center;">'+vstatus+'</div>'+      
+
     '</div>';
-    //'<div style="height:auto;width:100%;border:0px solid green;">'+getData_conso(pos)+'</div>';  
-    document.getElementById('conso_precinct_body').innerHTML+=dtl2;  
-    lctr++;
-    if(lctr > 40){
-      alert('kitakits: '+brgyDesc);
-      do_page();
-      lctr=10;
-    }
-  }
     
-  function do_page(){
+    lctr++;
+    if(lctr > 34){
+      //alert('kitakits: '+brgyDesc);      
+      do_header_conso_precinct();
+      dtl_line='';
+      pg++;
+      lctr=9;
+    }
+    
+  }  
+
+  //document.getElementById('conso_precinct_body').innerHTML=dtl2;  
+    // the quick html2pdf
+  function do_header_conso_precinct(){
+    //alert('pg:'+pg+' lctr:'+lctr);
     var dtl=
-    '<div style="width:850px;height:1100px;page-break-after:always;position:relative;font-size:12px;font-weight:bold;padding:10px;margin:0 auto;margin-top:10px;margin-bottom:10px;color:black;border:1px solid gray;background:white;">'+    
-      '<div style="width:100%;height:80px;font-size:14px;border:1px solid red;">'+
+    '<div id="pg_repo_conso_precinct_'+pg+'" style="width:100%;height:'+pg_height+'px;page-break-after:always;position:relative;font-size:12px;font-weight:bold;padding:10px;margin:0 auto;margin-top:0px;margin-bottom:10px;color:black;border:0px solid violet;background:white;">'+    
+
+      '<div style="width:100%;height:80px;font-size:14px;border:0px solid red;background:none;">'+
         '<div style="width:100%;height:20px;text-align:left;font-weight:bold;">PRECINCTS STATUS GROUP BY BARANGAY - CONSOLIDATED</div>'+
         '<div style="width:100%;height:20px;text-align:left;">As of: '+vtime+', '+vdate+'</div>'+        
         '<div style="width:100%;height:20px;text-align:left;">'+jformatNumber(tot_votes)+' of '+jformatNumber(tot_regVoters)+' Registered Voters</div>'+
       '</div>'+
     
-      '<div style="width:100%;height:50px;margin-top:0px;font-size:14px;border:1px solid green;">'+
-        '<div style="float:left;height:100%;width:30%;padding:4px;border:1px solid black;text-align:center;">BARANGAY NAME</div>'+
-        '<div style="float:left;height:100%;width:30%;padding:4px;border:1px solid black;text-align:left;">TOTALS</div>'+
-        '<div style="float:left;height:100%;width:10%;padding:4px;border:1px solid black;text-align:left;">POLITICAL</div>'+
-        '<div style="float:left;height:100%;width:30%;padding:4px;border:1px solid black;text-align:center;">PERCENTAGE</div>'+
-      '<div>'+
+      '<div style="width:100%;height:50px;margin-top:0px;font-size:14px;border:1px solid black;background:none;">'+
 
-      '<div id="conso_precinct_body" style="width:100%;height:935px;margin-top:60px;font-size:14px;border:1px solid pink;"></div>'+
+        '<div style="float:left;height:100%;width:30%;padding:14px 0 0 0;border:1px solid black;text-align:center;">BARANGAY NAME</div>'+
+
+        '<div style="float:left;height:100%;width:30%;padding:0px;border:0px solid black;">'+
+          '<div style="height:50%;width:100%;padding:3px 0 0 0;border:1px solid black;text-align:center;">T O T A L S</div>'+
+          '<div style="height:50%;width:100%;padding:0px;border:0px solid black;text-align:center;">'+
+            '<div style="float:left;height:100%;width:33%;padding:3px 0 0 0;border:1px solid black;text-align:center;">VOTERS</div>'+
+            '<div style="float:left;height:100%;width:34%;padding:3px 0 0 0;border:1px solid black;text-align:center;">VOTES</div>'+
+            '<div style="float:left;height:100%;width:33%;padding:3px 0 0 0;border:1px solid black;text-align:center;">CLUSTER</div>'+
+          '</div>'+
+        '</div>'+
+
+        '<div style="float:left;height:100%;width:10%;padding:14px;border:1px solid black;text-align:center;">PERC</div>'+
+
+        '<div style="float:left;height:100%;width:30.004%;padding:0px;border:0px solid black;">'+
+          '<div style="height:50%;width:100%;padding:3px 0 0 0;border:1px solid black;text-align:center;">C L U S T E R</div>'+
+          '<div style="height:50%;width:100%;padding:0px;border:0px solid black;text-align:center;">'+
+            '<div style="float:left;height:100%;width:33%;padding:3px 0 0 0;border:1px solid black;text-align:center;">OPEN</div>'+
+            '<div style="float:left;height:100%;width:34%;padding:3px 0 0 0;border:1px solid black;text-align:center;">CLOSED</div>'+
+            '<div style="float:left;height:100%;width:33%;padding:3px 0 0 0;border:1px solid black;text-align:center;">STATUS</div>'+
+          '</div>'+
+        '</div>'+
+
+      '</div>'+
+      
+      '<div style="width:100%;height:auto;margin-top:2px;font-size:14px;border:0px solid brown;">'+dtl_line+'</div>'+
+
     '<div>';
-    document.getElementById('main_repo_conso_precinct').innerHTML=dtl;
+    document.getElementById('main_repo_conso_precinct').innerHTML+=dtl;    
   }
 
-  function xxdo_header(){
-    var dtl=
-    '<div style="width:100%;height:80px;font-size:14px;border:1px solid red;">'+
-      '<div style="width:100%;height:20px;text-align:left;font-weight:bold;">PRECINCTS STATUS GROUP BY BARANGAY - CONSOLIDATED</div>'+
-      '<div style="width:100%;height:20px;text-align:left;">As of: '+vtime+', '+vdate+'</div>'+        
-      '<div style="width:100%;height:20px;text-align:left;">'+jformatNumber(tot_votes)+' of '+jformatNumber(tot_regVoters)+' Registered Voters</div>'+
-    '</div>'+
+}
+
+function getData_conso_precinct(brgyCode){  
+  var voters=0;
+  var clusters=0;
+  var votes=0;
+  var vopen=0;
+  var vclosed=0;
   
-    '<div style="width:100%;height:50px;margin-top:0px;font-size:14px;border:1px solid green;">'+
-      '<div style="float:left;height:100%;width:30%;padding:4px;border:1px solid black;text-align:center;">BARANGAY NAME</div>'+
-      '<div style="float:left;height:100%;width:30%;padding:4px;border:1px solid black;text-align:left;">TOTALS</div>'+
-      '<div style="float:left;height:100%;width:10%;padding:4px;border:1px solid black;text-align:left;">POLITICAL</div>'+
-      '<div style="float:left;height:100%;width:30%;padding:4px;border:1px solid black;text-align:center;">PERCENTAGE</div>'+
-    '<div>'+
+  for(var i=0;i<DB_CLUSTER.length;i++){
+    if(DB_CLUSTER[i]['brgyCode'] != brgyCode){ continue; }
 
-    '<div style="width:100%;height:700px;margin-top:60px;font-size:14px;border:1px solid coral;">';
-    document.getElementById('conso_precinct_head').innerHTML+=dtl;
+    voters+=parseInt(DB_CLUSTER[i]['regVoters']);
+    clusters+=parseInt(DB_CLUSTER[i]['prec_len']);
+
+    vopen++;
+    if(parseInt(DB_CLUSTER[i]['status'])==1){
+      vclosed++;
+    }
+  }
+  
+  for(var i=0;i<DB_TRAN_VOTES.length;i++){
+    if(DB_TRAN_VOTES[i]['brgyCode'] != brgyCode){ continue; }
+    votes+=parseInt(DB_TRAN_VOTES[i]['votes']);
   }
 
+  let ob={
+    "voters":voters,
+    "votes":votes,
+    "clusters":clusters,
+    "vopen":vopen,
+    "vclosed":vclosed
+  }
+  
+  return ob;
 }
 
--- repo/r_consolidated.js
+++ repo/r_consolidated.js
@@ -10,11 +10,12 @@ function proc_consolidated(){
 function print_proc_consolidated(){    
   //JBE_POPUP_CLOSE();
 
-  init_report('Partial and Unofficial Report - Consolidated',[84.5,8.5,1]);
+  init_report('Partial and Unofficial Report - Consolidated',[101.15,8.5,1]);
   var vdate = JBE_DATE_FORMAT(new Date());
   var vtime = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
 
-  var paper_width=8050;
+  var pg_width=9700;
+  var pg_height=850-60;
   
   var tot_votes=0;
   for(var k=0;k<DB_TRAN_VOTES.length;k++){
@@ -31,54 +32,126 @@ function print_proc_consolidated(){
   
   
   var dtl=
-  '<div id="preview_repo" class="cls_preview_repo" style="width:'+(paper_width-0)+'px;height:auto;font-size:12px;font-weight:bold;margin-bottom:10px;padding:10px;color:black;border:0px solid red;background:white;">'+
-  '</div>';
+  '<div id="main_repo_consolidated" class="cls_preview_repo" style="width:'+(pg_width-0)+'px;height:auto;font-size:12px;font-weight:bold;margin-bottom:0px;padding:10px;color:black;border:0px solid red;background:none;">'+     
+  '</div>';  
+  document.getElementById('prn_div').innerHTML=dtl; 
   
-  document.getElementById('prn_div').innerHTML=dtl;
+  var pg=0;
+  var lctr=0;
+  var dtl2='';
 
-
-  dtl=
-  //'<div class="cls_preview_repo" style="width:'+(paper_width-0)+'px;height:auto;position:relative;font-size:12px;font-weight:bold;margin-bottom:10px;padding:10px;color:black;border:0px solid gray;background:white;">'+
-    '<div style="width:100%;height:80px;border:0px solid violet;padding:0px;">'+    
-      '<div style="width:100%;height:20px;text-align:left;font-weight:bold;">PARTIAL AND UNOFFICIAL RESULTS -  CONSOLIDATED</div>'+
-      '<div style="width:100%;height:20px;text-align:left;">As of: '+vtime+', '+vdate+'</div>'+        
-      '<div style="width:100%;height:20px;text-align:left;">'+jformatNumber(tot_votes)+' of '+jformatNumber(tot_regVoters)+' Registered Voters</div>'+    
-    '</div>';
-  
+  do_header_consolidated();
   
   var aryPosition=DB_POSITION;
   //alert(aryPosition.length);
+  
   for(var i=0;i<aryPosition.length;i++){
     //alert(pos);
     var pos=aryPosition[i]['pos'];	
     var posname=aryPosition[i]['descrp'];	
-    if(aryPosition[i]['hide'] == 1) { continue; }  // hide dont display
-    //if(parseInt(pos) != 10) { continue; }    
-    //alert(pos);
-    dtl+=
-    '<div class="cls_line_pos" style="page-break-inside: avoid; height:25px;width:'+(paper_width-20)+'px;text-align:left;margin-top:20px;padding:4px;border:1px solid black;background:none;">FOR '+posname.toUpperCase()+'</div>'+
-    '<div style="height:25px;width:100%;border:0px solid blue;background:none;">'+      
-      '<div style="float:left;height:100%;width:60px;padding:4px;border:1px solid black;text-align:center;">RANK</div>'+
-      '<div style="float:left;height:100%;width:210px;padding:4px;border:1px solid black;text-align:left;">CANDIDATE</div>'+
-      '<div style="float:left;height:100%;width:200px;padding:4px;border:1px solid black;text-align:left;">POLITICAL PARTY</div>'+
-      '<div style="float:left;height:100%;width:120px;padding:4px;border:1px solid black;text-align:center;">PERCENTAGE</div>';
-      var dtl2='';
-      for(var ji=0;ji<tmp_ref_brgy.length;ji++){
-        dtl2+='<div style="float:left;height:100%;width:120px;text-align:center;padding:4px 0 0 0;border:1px solid black;background:none;">'+tmp_ref_brgy[ji]['brgyDesc']+'</div>';
+    if(parseInt(aryPosition[i]['hide']) == 1) { continue; }  // hide dont display
+    
+    do_subheader_consolidated();
+    lctr=lctr+2;
+    
+    //var xdtl_line='<div style="height:25px;width:100%;text-align:left;background:none;">jbe line:'+i+'</div>';
+    //document.getElementById('repo_consolidated_body_'+pg).innerHTML+=xdtl_line;
+    
+    var aryDATUM=getData_conso(pos);
+    var aryDATA=aryDATUM[0];
+    var aryDATAbrgy=aryDATUM[1];
+    //alert('pos:'+posname+' array len:'+aryDATA.length);
+    dtl_line='';
+    
+    for(var ii=0;ii<aryDATA.length;ii++){    
+      var candi_no=aryDATA[ii]['candi_no'];
+      //alert('lk: '+i+' = '+candi_no);
+      var candi_name=JBE_GETFLD('name',DB_CANDIDATE,'code',candi_no).toUpperCase();
+      var partyno=JBE_GETFLD('partyno',DB_CANDIDATE,'code',candi_no).toUpperCase();
+      var partyname=JBE_GETFLD('partyname',DB_PARTY,'partyno',partyno).toUpperCase();
+      var perc=aryDATA[ii]['perc']+'%';
+      var candi_votes=aryDATA[ii]['candi_votes'];
+      
+      dtl_line=
+      '<div style="height:25px;width:100%;background:none;">'+
+        '<div style="float:left;height:100%;width:0.705%;border:1px solid black;padding:3px;text-align:center;">'+(ii+1)+'. '+'</div>'+
+        '<div style="float:left;height:100%;width:2.5%;border:1px solid black;padding:3px;text-align:left;">'+candi_name+'</div>'+
+        '<div style="float:left;height:100%;width:2.4%;border:1px solid black;padding:3px;text-align:left;">'+partyname+'</div>'+
+        '<div style="float:left;height:100%;width:1.4%;border:1px solid black;padding:3px;text-align:center;">'+perc+'</div>';
+        
+        var dtl2='';
+        
+        for(var ji=0;ji<aryDATAbrgy.length;ji++){      
+          if(aryDATAbrgy[ji]['candi_no'] != candi_no){ continue; }
+          var votes=aryDATAbrgy[ji]['brgyVotes'];          
+          dtl2+='<div style="float:left;height:100%;width:1.5%;text-align:center;padding:5px;border:1px solid black;background:none;">'+iif(votes==0,'',jformatNumber(votes))+'</div>';
+        }             
+        dtl_line+=dtl2+
+        
+        '<div style="float:left;height:100%;width:1.5%;text-align:center;padding:5px;border:1px solid black;background:none;">'+jformatNumber(candi_votes)+'</div>'+
+      '</div>';
+
+      /*
+      dtl_line=
+      '<div style="height:25px;width:100%;text-align:left;background:none;">'+
+        '<div style="float:left;height:100%;width:2.2%;border:1px solid black;padding:3px;text-align:left;">jbe line:'+ii+' lctr:'+lctr+'</div>'+        
+      '</div>';
+      */
+
+      //dtl_line=dtl+dtl_line;
+      //alert('pg:'+pg);
+      //document.getElementById('repo_consolidated_body'+pg).innerHTML+=dtl_line;        
+      lctr++;    
+      if(lctr > 31){
+        //alert('kitakits: '+posname);  
+        do_header_consolidated();
       }
-      dtl+=dtl2+
-
-      '<div style="float:left;height:100%;width:120px;padding:4px;border:1px solid black;text-align:center;">TOTAL VOTES</div>'+
-    '</div>'+
-    '<div style="height:auto;width:100%;border:0px solid green;">'+getData_conso(pos)+'</div>';    
-
+      document.getElementById('repo_consolidated_body'+pg).innerHTML+=dtl_line;        
+    }    
+    
   }
-  //dtl+='</div>';  
-  document.getElementById('preview_repo').innerHTML=dtl; 
 
-  document.getElementById('prn_div').style.width=paper_width+'px';  
-  document.getElementById('prn_div').style.fontSize='12px';
+  document.getElementById('prn_div').style.width=pg_width+'px';  
+  document.getElementById('prn_div').style.fontSize='11px';
   //document.getElementById('prn_div').style.border='1px solid red';
+
+  function do_header_consolidated(){
+    pg++;
+    //alert('pg:'+pg+' lctr:'+lctr);
+    var dtl=
+    '<div id="pg_repo_consolidated'+pg+'" style="width:100%;height:'+pg_height+'px;page-break-after:always;position:relative;font-size:12px;font-weight:bold;padding:10px;margin:0 auto;margin-top:0px;margin-bottom:10px;color:black;border:0px solid violet;background:white;">'+    
+      '<div style="width:100%;height:80px;font-size:14px;border:0px solid red;background:none;">'+
+        '<div style="width:100%;height:20px;text-align:left;font-weight:bold;">PARTIAL AND UNOFFICIAL RESULTS -  CONSOLIDATED</div>'+
+        '<div style="width:100%;height:20px;text-align:left;">As of: '+vtime+', '+vdate+'</div>'+        
+        '<div style="width:100%;height:20px;text-align:left;">'+jformatNumber(tot_votes)+' of '+jformatNumber(tot_regVoters)+' Registered Voters</div>'+    
+      '</div>'+   
+      '<div id="repo_consolidated_body'+pg+'" style="width:100%;height:'+(pg_height-100)+'px;margin-top:0px;font-size:14px;border:0px solid white;"></div>'+
+    '</div>';
+    document.getElementById('main_repo_consolidated').innerHTML+=dtl;    
+    lctr=6;
+    //alert(lctr);
+  }
+
+  function do_subheader_consolidated(){
+    //alert('sub pg:'+pg+' lctr:'+lctr);
+    var vdtl=
+    '<div id="sub_pg_repo_consolidated_'+pg+'" style="width:100%;height:50px;border:0px solid coral;margin-top:10px;padding:0px;">'+    
+      '<div class="cls_line_pos" style="height:50%;width:100%;text-align:left;padding:3px;border:1px solid black;background:none;">FOR '+posname.toUpperCase()+'</div>'+
+      '<div style="height:50%;width:100%;border:0px solid blue;background:none;">'+      
+        '<div style="float:left;height:100%;width:0.705%;padding:3px;border:1px solid black;text-align:center;">RANK</div>'+
+        '<div style="float:left;height:100%;width:2.5%;padding:3px;border:1px solid black;text-align:left;">CANDIDATE</div>'+
+        '<div style="float:left;height:100%;width:2.4%;padding:3px;border:1px solid black;text-align:left;">POLITICAL PARTY</div>'+
+        '<div style="float:left;height:100%;width:1.4%;padding:3px;border:1px solid black;text-align:center;">PERCENTAGE</div>';
+        var vdtl3='';
+        for(var ji=0;ji<tmp_ref_brgy.length;ji++){
+          vdtl3+='<div style="float:left;height:100%;width:1.5%;text-align:center;padding:3px 0 0 0;border:1px solid black;background:none;">'+tmp_ref_brgy[ji]['brgyDesc']+'</div>';
+        }
+        vdtl+=vdtl3+
+        '<div style="float:left;height:100%;width:1.5%;padding:3px;border:1px solid black;text-align:center;">TOTAL VOTES</div>'+
+      '</div>'+
+    '</div>';
+    document.getElementById('repo_consolidated_body'+pg).innerHTML+=vdtl;        
+  }
 }
 
 
@@ -158,7 +231,14 @@ function getData_conso(pos){
   }
   
   var dtl='';  
+
   aryData.sort(sortByMultipleKey(['*candi_votes']));
+
+  var retArray=[];
+  retArray[0]=aryData;
+  retArray[1]=aryDataBrgy;
+  
+  return retArray;
   
   for(var i=0;i<aryData.length;i++){   
     var candi_no=aryData[i]['candi_no'];
@@ -169,26 +249,37 @@ function getData_conso(pos){
     var perc=aryData[i]['perc']+'%';
     var candi_votes=aryData[i]['candi_votes'];
     //alert(candi_name);
+    /*
+    '<div style="float:left;height:100%;width:0.6%;padding:3px;border:1px solid black;text-align:center;">RANK</div>'+
+    '<div style="float:left;height:100%;width:2.2%;padding:3px;border:1px solid black;text-align:left;">CANDIDATE</div>'+
+    '<div style="float:left;height:100%;width:2%;padding:3px;border:1px solid black;text-align:left;">POLITICAL PARTY</div>'+
+    '<div style="float:left;height:100%;width:1.3%;padding:3px;border:1px solid black;text-align:center;">PERCENTAGE</div>';
+    */
     dtl+=
     '<div style="height:25px;width:100%;background:none;">'+
-      '<div style="float:left;height:100%;width:60px;border:1px solid black;padding:4px;text-align:center;">'+(i+1)+'. '+'</div>'+
-      '<div style="float:left;height:100%;width:210px;border:1px solid black;padding:4px;text-align:left;">'+candi_name+'</div>'+
-      '<div style="float:left;height:100%;width:200px;border:1px solid black;padding:4px;text-align:left;">'+partyname+'</div>'+
-      '<div style="float:left;height:100%;width:120px;border:1px solid black;padding:4px;text-align:center;">'+perc+'</div>';
+      '<div style="float:left;height:100%;width:0.6%;border:1px solid black;padding:3px;text-align:center;">'+(i+1)+'. '+'</div>'+
+      '<div style="float:left;height:100%;width:2.2%;border:1px solid black;padding:3px;text-align:left;">'+candi_name+'</div>'+
+      '<div style="float:left;height:100%;width:2%;border:1px solid black;padding:3px;text-align:left;">'+partyname+'</div>'+
+      '<div style="float:left;height:100%;width:1.3%;border:1px solid black;padding:3px;text-align:center;">'+perc+'</div>';
       
       var dtl2='';
       
       for(var ji=0;ji<aryDataBrgy.length;ji++){      
         if(aryDataBrgy[ji]['candi_no'] != candi_no){ continue; }
         var votes=aryDataBrgy[ji]['brgyVotes'];          
-        dtl2+='<div style="float:left;height:100%;width:120px;text-align:center;padding:5px;border:1px solid black;background:none;">'+iif(votes==0,'',jformatNumber(votes))+'</div>';
+        dtl2+='<div style="float:left;height:100%;width:1.5%;text-align:center;padding:5px;border:1px solid black;background:none;">'+iif(votes==0,'',jformatNumber(votes))+'</div>';
       }             
       dtl+=dtl2+
       
-      '<div style="float:left;height:100%;width:120px;text-align:center;padding:5px;border:1px solid black;background:none;">'+jformatNumber(aryData[i]['candi_votes'])+'</div>'+
+      '<div style="float:left;height:100%;width:1.5%;text-align:center;padding:5px;border:1px solid black;background:none;">'+jformatNumber(aryData[i]['candi_votes'])+'</div>'+
     '</div>';
   }
-  return dtl;
+
+  let ob_data={
+    "dtl":dtl,
+    "ctr":i
+  };
+  return ob_data;
 
   function update_brgy(candi_no,brgyCode,brgyVotes){
     //brgyVotes=8888;
