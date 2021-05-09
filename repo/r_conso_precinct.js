5967e8ffd92d6ae56f492c58049829ea575693c4
HAT Software Development Services <75292269+hatsoftware@users.noreply.github.com>
2021-05-09 09:58:39 +0800

    Add files via upload

--- repo/r_conso_precinct.js
+++ repo/r_conso_precinct.js
@@ -60,6 +60,7 @@ function print_proc_conso_precinct(){
     var vopen=result['vopen'];
     var vclosed=result['vclosed'];
     var vstatus='OPEN';
+    alert(perc);
     if(perc==100){ vstatus='CLOSED'; }
     perc=the_perc(perc);
 
